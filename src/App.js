d11ad00d1990ebdeb789fe52143003a8c0fa4b17
Ivan Višnjić <ivanvisnjic2@gmail.com>
2023-07-25 20:32:33 +0200

    first layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
 
+function MyButton() {
+  return <button className="item btn">I'm a button</button>;
+}
+
 function App() {
-  return <div>Hello Chat</div>;
+  return (
+    <div className="main">
+      <div className="header">
+        <h1>Hello Chat</h1>
+      </div>
+      <div className="main-area">
+        <div className="users">
+          <p>Ivan</p>
+          <p>Manfres</p>
+          <p>fritz</p>
+        </div>
+        <div className="text-msgs">
+          <div className="text-msg own-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+          <div className="text-msg">lorem</div>
+        </div>
+      </div>
+      <div className="input-area">
+        <textarea rows={3} className="text-input" />
+        <MyButton />
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
